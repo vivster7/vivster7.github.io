a978d7fd626f25397c2021f5dc8a20cbadc12463
Vivek Dasari <vivdasari@gmail.com>
2020-05-06 09:50:50 -0700

    add boxes-and-arrows

---
+++ posts/boxes-and-arrows/precache-manifest.1d2cdaa734c03b4275e2c57b318cfdb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f5487ffeffeff8f5a5b6e22c186e3a",
+    "url": "/posts/boxes-and-arrows/index.html"
+  },
+  {
+    "revision": "0f8a1b516c35baa78cad",
+    "url": "/posts/boxes-and-arrows/static/css/main.4ad67b71.chunk.css"
+  },
+  {
+    "revision": "245e1437ef51e1118f0d",
+    "url": "/posts/boxes-and-arrows/static/js/2.57807c61.chunk.js"
+  },
+  {
+    "revision": "d0e4b0a3f84a7df5f3eec41b0a47276e",
+    "url": "/posts/boxes-and-arrows/static/js/2.57807c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8a1b516c35baa78cad",
+    "url": "/posts/boxes-and-arrows/static/js/main.68bc1da8.chunk.js"
+  },
+  {
+    "revision": "51bb2705c7780c49044d",
+    "url": "/posts/boxes-and-arrows/static/js/runtime-main.5beabb4a.js"
+  },
+  {
+    "revision": "bf38b4f0e1713aa9264717000c5062b1",
+    "url": "/posts/boxes-and-arrows/static/media/minus.bf38b4f0.svg"
+  },
+  {
+    "revision": "5af51bb4b15311f67a2cc1a614680485",
+    "url": "/posts/boxes-and-arrows/static/media/plus.5af51bb4.svg"
+  }
+]);
